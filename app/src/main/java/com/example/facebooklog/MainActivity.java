8673733ffc135d5a60ea9dcb0ff9eba181a65574
ayeshajagirdar <ayeshajagirdarm88@gmail.com>
2021-06-17 20:12:25 +0530

    hdsahuiah

---
+++ app/src/main/java/com/example/facebooklog/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.facebooklog;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView info;
+    private ImageView profile;
+    private LoginButton login;
+    CallbackManager callbackManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        info = findViewById(R.id.info);
+        profile = findViewById(R.id.profile);
+        login = findViewById(R.id.login);
+
+        callbackManager = CallbackManager.Factory.create();
+        login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("user id: " + loginResult.getAccessToken().getUserId());
+                String imageURL = "https://graph.facebook.com/" + loginResult.getAccessToken().getUserId() + "/picture?return_ssl_resouces=1";
+                Picasso.get().load(imageURL).into(profile);
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        startActivity(new Intent(MainActivity.this , MainActivity2.class));
+    }
+}
